febd5cd04b87a6e8c539f1a186381abb4f650305
sergiocastro85 <sergiocastro86@gmail.com>
2022-04-26 00:26:40 -0500

    Ejemplo Calse con pitido NetCore

---
+++ netCore/NetCore/Clase/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Escuela
+    {
+
+        public string nombre;
+
+        public string direccion;
+
+        public int anoFundacion;
+
+        public string ceo;
+
+        public void Timbrar()
+        {
+            Console.Beep(10000, 30000);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var miEscuela = new Escuela();
+
+
+            miEscuela.nombre = "Pruebas Personales";
+            miEscuela.direccion = "Calle 92 83-46";
+            miEscuela.anoFundacion = 2001;
+            Console.WriteLine("timbre");
+            
+            miEscuela.Timbrar();
+
+        }   
+            //Console.WriteLine("Hello World!");
+
+
+     }
+    
+}
